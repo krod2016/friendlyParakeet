9f4dc7ca223543f5f4690be4e5afee6cd2343d7e
krod2016 <katrinajrodriguez1993@gmail.com>
2021-10-10 12:15:08 -0500

    added if statements

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,41 +4,81 @@ var lwrChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", 
 var specChar = ["!", "#", "$", "%", "&", "*", "-", ".", "?", "@",]
 var ranCharPool = []
 
-//parameters for random password
-function getUserData() {
-  var userNum = confirm(
-    'Would you like to use a number?'
-  )
-  var userCap = confirm(
-    'Would you like to use a capital letter?'
-  )
-  var userLwr = confirm(
-    'Would you like to use a lower case number?'
-  )
-  var userSpec = confirm(
-    'Would you like to use a special character?'
-  )
-  var userInput = window.prompt (
-    'Please choose your password character length varying from 8-20.'
-  )
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
 }
 
-//using user inputs, translating to integer
-if (parseInt(userInput) >=8 ||parseInt(userInput) <=1) {
+//parameters for random password
+function getUserData() {
+  var userNum = confirm( 'Would you like to use a number?');
+    if (userNum) {
+      passwordSet += selectionOfCharachters.userNum;
+    }
+  var userCap = confirm('Would you like to use a capital letter?');
+    if (userCap) {
+      passwordSet += selectionOfCharachters.userCap;
+    }
+  var userLwr = confirm('Would you like to use a lower case number?');
+  if (userLwr) {
+    passwordSet += selectionOfCharachters.userLwr;
+  }
+  var userSpec = confirm('Would you like to use a special character?');
+  if (userSpec) {
+    passwordSet += selectionOfCharachters.userSpec;
+  }
+  var userInput = window.prompt ('Please choose your password character length varying from 8-20.');
+  if (userInput) {
+    passwordSet += selectionOfCharachters.userInput;
+  }
+}
+
+// Using user inputs, translating to integer
+if (parseInt(userInput) <=8) {
   alert('Please select a criteria value.')
 }
 
+// If user follows parameters
+if (userNum) {
+  ranCharPool = ranCharPool.concat(numChar)
+}
+if (userCap) {
+  ranCharPool = ranCharPool.concat(capChar)
+}
+if (userLwr) {
+  ranCharPool = ranCharPool.concat(lwrChar)
+}
+if (userSpec) {
+  ranCharPool = ranCharPool.concat(specChar)
+}
+
+// If user doesn't follow parameters
+if (userNum === false && userCap === false && userLwr === false && userSpec === false) {
+  alert("Please choose a valid response.")
+  return null;
+}
+
+var ranPassword = ''
+for (let i =0; i < length; i++) {
+  ranPassword += ranCharPool (
+  Math.floor(math.random) * ranCharPool.length
+  )
+  return password;
+}
 
 // Get references to the #generate element
 document.querySelector("#generate").addEventListener("click", getUserData);
 
-// Write password to the #password input
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
-//   passwordText.value = password;
-// }
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
-// // Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
+// End of function
+writePassword()
+
+
+
+
+
 
