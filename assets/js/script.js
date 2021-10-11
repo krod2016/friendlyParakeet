e28e3f433af5ca2cf9f51d6ef6466c2cd66171a5
krod2016 <katrinajrodriguez1993@gmail.com>
2021-10-11 18:43:54 -0500

    updated min and max ranges.

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,10 +17,12 @@ function writePassword() {
 //parameters for random password
 function generatePassword() {
   var passwordSet = "";
-  var length = prompt("How many charachters for your password?");
-  length = parseInt(length)
+  var length = prompt("How many characters for your password?");
+  var lengthInt = parseInt(length)
 
-  if (length >= 8 && length <= 128) {
+  console.log(lengthInt);
+
+  if (lengthInt >= 8 && lengthInt <= 128) {
     var numberChar = confirm("Would you like to use a number? Choose 'OK' for yes, and 'Cancel' for no.");
     if (numberChar) {
       passwordSet += selectionOfCharacters.numberChar;
@@ -37,15 +39,13 @@ function generatePassword() {
     if (specialChar) {
       passwordSet += selectionOfCharacters.specialChar;
     }
+    var ranPassword = ['']
+    for (let i = 0; i < length; i++) {
+      ranPassword += selectionOfCharacters.ranCharPool[Math.floor(Math.random() * selectionOfCharacters.ranCharPool.length)] /* a randomly selected character from passwordSet */
+    }
+    return ranPassword
   }
-
   console.log(passwordSet) //string with the character pool.
-
-  var ranPassword = ['']
-  for (let i = 0; i < length; i++) {
-    ranPassword += selectionOfCharacters.ranCharPool[Math.floor(Math.random() * selectionOfCharacters.ranCharPool.length)] /* a randomly selected character from passwordSet */
-  }
-  return ranPassword
 }
 
 document.getElementById('generate').addEventListener("click", writePassword);
